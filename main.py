3e56758db1747869304d0c91cb5cb5579fa4dab3
emilymarquessalum <emilymarquessalum@gmail.com>
2025-12-16 21:57:54 -0300

    updated if

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def generate_spell(request: SpellRequest):
 
 
 
-if (__name__ == "__main__" ) or True:
+if (__name__ == "__main__" ) :
     
     #uvicorn.run("main:app", host="127.0.0.1", port=8000)
     
